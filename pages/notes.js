9fc6e3067a9d8f8fc39b2365fab1ec2146ab6d85
vladislav <vlad.kovalenko@aviasales.ru>
2021-03-10 01:13:26 +0700

    about

--- pages/notes.js
+++ pages/notes.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import PageDescription from "../src/page_description"
-import {Divider} from 'antd'
+import {Col, Divider} from 'antd'
 
 import {Note, Notes} from '../src/note'
 
 const pageDescriptionText = 
   <article>
-    <p>Here I write small tween-like notes and postmortems from my experience. They will serve me as reminders and may be helpful to others.</p>
+    <p>Here I write small tweet-like notes and postmortems from my experience. They will serve me as reminders and may be helpful to others.</p>
     <p>Common notes marked as ✅, postmortems 😠 and ideas 💡</p>
+    <p>And important. If you are disagree please PM me, I'd like to discuss.</p>
   </article>
   
   export default () => 
-    <div>
+    <Col offset={6}>
         <PageDescription children={pageDescriptionText}/> 
         <Divider/>
 
@@ -23,4 +24,4 @@ const pageDescriptionText =
             </>  
         )
         }
-   </div>
+   </Col>
